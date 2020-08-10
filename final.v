05cd99379d42b37a9097b9cb504e9ac43f6770c8
millert4atwit <60189060+millert4atwit@users.noreply.github.com>
2020-08-09 20:12:34 -0400

    changing temp almost works

--- final.v
+++ final.v
@@ -1,4 +1,4 @@
-module final (input clk, A, B, C, D, output Z, output [7:0] H3, H2, H1, H0, G2, G1, G0);
+module final (input clk, A, B, C, D, output Z, output [7:0] H3, H2, H1, H0);
 	//A is for turning the oven on and off(display time or turn the oven on)
 	//B raises temp
 	//C lowers temp
@@ -10,14 +10,18 @@ module final (input clk, A, B, C, D, output Z, output [7:0] H3, H2, H1, H0, G2, 
 	reg new_clk = 0;										// Clock to be used
 	
 	reg [3:0] Q3, Q2, Q1, Q0 = 0;
-//	reg [4:0] G3, G2, G1, G0 = 0;
 	
 	reg [16:0] clock = 0;								// Need register for clock to display time. Time will be an integer of seconds
-	reg [9:0] temp = 0;									// Need register for clock to display temp
+	reg [10:0] temp = 0;									// Need register for clock to display temp
 	reg [10:0]targetTemp = 357;
 	reg loopCount = 0;
 	reg [21:0] store = 0;
-
+	reg i = 0;
+	reg changeB = 0;
+	reg lastB = 0;
+	reg changeC = 0;
+	reg lastC = 0;
+	reg reset = 0;
 	// Put each register into the seven segment module
 	
 	
@@ -31,7 +35,6 @@ module final (input clk, A, B, C, D, output Z, output [7:0] H3, H2, H1, H0, G2, 
 		end
 	end
 	
-	
 	reg [1:0] current_state, next_state;					
 	localparam A00 = 0, A01 = 1, A10 = 2, A11 = 3;
 	
@@ -40,13 +43,28 @@ module final (input clk, A, B, C, D, output Z, output [7:0] H3, H2, H1, H0, G2, 
 	sevenseg s1(Q1, H1);
 	sevenseg s0(Q0, H0);
 	
+	always @ (negedge B) begin
+		changeB = changeB + 1;
+	end
+	always @ (negedge C) begin
+		changeC = changeC + 1;
+	end
+		
 
 	// Always block to control FSM based on clock cycle
 	always @ (posedge new_clk) begin
+		if (changeB != lastB) begin
+			targetTemp = targetTemp + (changeB - lastB);
+			lastB = changeB;
+		end
+		if (changeC != lastC) begin
+			targetTemp = targetTemp + (lastC - changeC);
+			lastC = changeC;
+		end
 		if (A == 0) begin
 			//current_state <= A10;
 			store = 0;
-			store[10:0] = targetTemp[10:0];
+			store = targetTemp;
 			repeat (10) begin
 				if (store[13:10] >= 5) begin
 					store[13:10] = store[13:10] + 3;
@@ -62,9 +80,17 @@ module final (input clk, A, B, C, D, output Z, output [7:0] H3, H2, H1, H0, G2, 
 			Q0 = store[13:10];
 			Q1 = store[17:14];
 			Q2 = store[21:18];
+			Q3 = 0;
+			i = 1;
 		end
-/*		else begin
-			current_state <= next_state;
+		else begin
+			if (i == 1) begin
+				i = 0;
+				Q0 = 0;
+				Q1 = 0;
+				Q2 = 0;
+				Q3 = 0;
+			end
 			if (A == 1) begin
 				Q0 = Q0 + 1;
 				if (Q0 == 10) begin
@@ -85,61 +111,50 @@ module final (input clk, A, B, C, D, output Z, output [7:0] H3, H2, H1, H0, G2, 
 					end
 				end
 			end
-		end */
+		end 
 	end
-
 	
 	
 	// Need to find next state
-	
+
+
 /*	always @ (*) begin
-		next_state = current_state;
-		if (B == 0) begin
-			targetTemp = targetTemp + 1;
-		end
-		else begin 
-			targetTemp = targetTemp;
-		end
-		if (C == 0) begin
-			targetTemp = targetTemp - 1;
-		end
-		else begin
-			case (current_state)
-				A00: begin
-					if (temp < targetTemp - 2) begin
-						temp = temp + 2;
-						next_state = A00;
-					end
-					else begin
-						temp = temp + 2;
-						next_state = A01;
-					end
+
+		case (current_state)
+			A00: begin
+				if (temp < targetTemp - 2) begin
+					temp = temp + 2;
+					next_state = A00;
 				end
-				A01: begin
-					if (loopCount == 1) begin
-						temp = temp - 1;
-						loopCount = 0;
-						next_state = A01;
-					end
-					if (temp > targetTemp - 2) begin
-						loopCount = 1;
-						next_state = A01;
-					end
-					else begin
-						next_state = A00;
-						loopCount = 0;
-					end
+				else begin
+					temp = temp + 2;
+					next_state = A01;
 				end
-				A10: begin
-					// In State 10, Oven is ON, preheating to the desired temp
+			end
+			A01: begin
+				if (loopCount == 1) begin
+					temp = temp - 1;
+					loopCount = 0;
+					next_state = A01;
 				end
-				A11: begin
-					// In State 11, Oven is ON, needs to bake for desired time
+				if (temp > targetTemp - 2) begin
+					loopCount = 1;
+					next_state = A01;
 				end
-			endcase	
-		end	
+				else begin
+					next_state = A00;
+					loopCount = 0;
+				end
+			end
+			A10: begin
+				// In State 10, Oven is ON, preheating to the desired temp
+			end
+			A11: begin
+				// In State 11, Oven is ON, needs to bake for desired time
+			end
+		endcase
+	current_state = next_state;	
 	end	*/	
-	
 	
 endmodule
 
